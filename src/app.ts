260f57d1e7fc465be4713e9364d04f1bfffb1787
Dennis Costa <dennismeira.costa@gmail.com>
2024-09-10 20:46:40 -0300

    prisma install, first settings and tests code preview

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
 import fastify from 'fastify'
-
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Dennis',
+        email: 'dennis@email.com',
+    }
+})
