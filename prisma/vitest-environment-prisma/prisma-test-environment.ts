b8d80c25a66c97eaf65081b01d576cebc1b62424
Dennis <dennismeira.costa@gmail.com>
2024-09-16 13:34:16 -0300

    npm scripts and prisma test environment setup - aulas 8 e 9

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config"
+import { execSync } from "node:child_process";
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest/environments';
+
+const prisma = new PrismaClient()
 
 const prismaEnvironment: Environment = {
     name: 'prisma',
     transformMode: 'ssr', // ou 'web', dependendo do seu caso
     async setup() {
-        console.log('Antes dos testes');
+        const schema = randomUUID()
+        const databaseURL = generateDatabaseURL(schema)
+
+        process.env.DATABASE_URL = databaseURL
+
+        execSync('npx prisma migrate deploy')
+
         return {
-            teardown() {
-            console.log('Depois dos testes');
+            async teardown() {
+                await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+                await prisma.$disconnect()
             },
         };
     },
 };
+
+function generateDatabaseURL(schema: string) {
+    if(!process.env.DATABASE_URL) {
+        throw new Error('Pleas provide a DATABASE_URL environment variable.')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+    url.searchParams.set('schema', schema)
+
+    return url.toString()
+}
 
 export default prismaEnvironment;
