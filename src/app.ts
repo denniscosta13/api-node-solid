fdff924506480b64d0375cc4437389fd82d16114
Dennis <dennismeira.costa@gmail.com>
2024-09-17 09:47:06 -0300

    refresh token controller and test

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { gymsRoutes } from './http/controllers/gyms/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { checkInsRoutes } from './http/controllers/checkins/routes'
 
 export const app = fastify()
@@ -13,7 +14,16 @@ export const app = fastify()
 //esse token nós podemos salvar no .env e utilizar qualquer um para desenvolvimento
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false,
+    },
+    sign: {
+        expiresIn: '10m'
+    }
 })
+
+app.register(fastifyCookie)
 
 //registra o plugin contendo as rotas da nossa aplicacao
 //por meio desse registro, nosso server ganha acesso as rotas definadas em routes.ts
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,12 +3,14 @@ import { register } from "./register-controller" // importa o controller respons
 import { authenticate } from "./authenticate-controller"
 import { profile } from "./profile-controller"
 import { verifyJwt } from "@/http/middlewares/verify-jwt"
+import { refresh } from "./refresh"
 
 
 //função ou plugin (fastify) que contem nossas rotas da aplicacao
 export async function  usersRoutes(app:FastifyInstance) {
     app.post('/users', register) //define cada rota e seu controller
     app.post('/sessions', authenticate)
+    app.patch('/token/refresh', refresh)
 
     /** Authenticated routes */
     app.get('/me', { onRequest: [verifyJwt] }, profile)
